2a4806fa37344c948e3dfd1e671a8ec3734d16e4
HanLi <729508792@qq.com>
2020-11-05 00:03:51 +0800

    第三周作业

---
+++ Week_03/src/test/java/com/lh/learning/LearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lh.learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
