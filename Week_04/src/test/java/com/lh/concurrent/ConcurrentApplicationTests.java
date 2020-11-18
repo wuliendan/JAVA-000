29a3a4a8028c63d45e75ceffabd942a5966b49fe
HanLi <729508792@qq.com>
2020-11-18 23:31:23 +0800

    第五周作业

---
+++ Week_04/src/test/java/com/lh/concurrent/ConcurrentApplicationTests.java
@@ -0,0 +1,13 @@
+package src.test.java.com.lh.concurrent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurrentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
